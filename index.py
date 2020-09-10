c04ca16af99e6748e7ae8acf5d1e7964813e92b1
jongmyoungkim <34468884+jongmyoungkim@users.noreply.github.com>
2020-09-10 11:26:20 +0900

    Add files via upload

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index'
+    return 'index - hello, world!'
 
 
 
@@ -19,15 +19,3 @@ def index():
 def hello(name=None):
     return render_template('hello.html', name=name)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
